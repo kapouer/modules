e85804e722d8ba8c92097840945895707253d023
Jérémy Lal <kapouer@melix.org>
2021-04-29 12:25:44 +0200

    async/await for tests

--- bundle/test/suite.js
+++ bundle/test/suite.js
@@ -1,27 +1,10 @@
 const should = require('should');
 const jsdom = require('jsdom');
 const Path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 const mkdirp = require('mkdirp');
 
 const bundledom = require('..');
-
-function runDom(htmlPath, data) {
-	return new Promise(function (resolve, reject) {
-		const virtualConsole = new jsdom.VirtualConsole();
-		virtualConsole.on('jsdomError', function (err) {
-			reject(err);
-		});
-		const dom = new jsdom.JSDOM(data.html, {
-			virtualConsole: virtualConsole,
-			url: 'file://' + Path.resolve(htmlPath),
-			runScripts: "dangerously",
-			resources: "usable"
-		});
-		dom.window.eval(data.js);
-		resolve(dom.window.document);
-	});
-}
 
 describe("test suite", function () {
 	this.timeout(10000);
@@ -43,329 +26,295 @@ describe("test suite", function () {
 		}
 	});
 
-	it('should concat js for legacy scripts', function () {
-		return bundledom('test/fixtures/concat.html', {
+	it('should concat js for legacy scripts', async function () {
+		const data = await bundledom('test/fixtures/concat.html', {
 			exclude: [],
 			concatenate: true
-		}).then(function (data) {
-			data.should.have.property('js');
-			data.js.should.containEql("this.towin = true;");
-			data.should.have.property('html');
 		});
+		data.should.have.property('js');
+		data.js.should.containEql("this.towin = true;");
+		data.should.have.property('html');
 	});
 
-	it('should work without anything to do', function () {
-		return bundledom('test/fixtures/none.html', {
+	it('should work without anything to do', async function () {
+		const data = await bundledom('test/fixtures/none.html', {
 			exclude: []
-		}).then(function (data) {
-			data.should.have.property('js');
-			data.should.have.property('css');
-			data.should.have.property('html');
 		});
+		data.should.have.property('js');
+		data.should.have.property('css');
+		data.should.have.property('html');
 	});
 
-	it('should support es modules', function () {
-		return bundledom('test/fixtures/esm.html', {
+	it('should support es modules', async function () {
+		const data = await bundledom('test/fixtures/esm.html', {
 			exclude: [],
 			concatenate: true
-		}).then(function (data) {
-			data.scripts.should.eql(['mod.js', 'depmod.js']);
-			data.should.have.property('js');
-			data.js.trim().should.startWith('(function (');
-			data.js.includes('emptyObject').should.be.true();
-			data.should.have.property('html');
 		});
+		data.scripts.should.eql(['mod.js', 'depmod.js']);
+		data.should.have.property('js');
+		data.js.trim().should.startWith('(function (');
+		data.js.includes('emptyObject').should.be.true();
+		data.should.have.property('html');
 	});
 
-	it('should support es modules with browser prefix resolver', function () {
-		return bundledom('test/fixtures/esm-browser.html', {
+	it('should support es modules with browser prefix resolver', async function () {
+		const data = await bundledom('test/fixtures/esm-browser.html', {
 			modulesPrefix: '/',
 			modulesRoot: "test",
 			exclude: [],
 			concatenate: true
-		}).then(function (data) {
-			data.scripts.should.eql([
-				'mod-browser.js',
-				'../node_modules/redirect-exports/src/index.js'
-			]);
-			data.should.have.property('js');
-			data.js.trim().should.startWith('(function (');
-			data.js.includes('var test = 1;').should.be.true();
-			data.should.have.property('html');
 		});
+		data.scripts.should.eql([
+			'mod-browser.js',
+			'../node_modules/redirect-exports/src/index.js'
+		]);
+		data.should.have.property('js');
+		data.js.trim().should.startWith('(function (');
+		data.js.includes('var test = 1;').should.be.true();
+		data.should.have.property('html');
 	});
 
-	it('should support legacy-resolved modules', function () {
+	it('should support legacy-resolved modules', async function () {
 		process.env.BROWSERSLIST = "last 1 chrome version";
-		return bundledom('test/fixtures/legacy.html', {
-			root: "test/fixtures",
-			modulesPrefix: '/',
-			modulesRoot: "test",
-			exclude: [],
-			concatenate: true
-		}).then(function (data) {
+		try {
+			const data = await bundledom('test/fixtures/legacy.html', {
+				root: "test/fixtures",
+				modulesPrefix: '/',
+				modulesRoot: "test",
+				exclude: [],
+				concatenate: true
+			});
 			data.scripts.sort().should.eql([
 				'mod.js',
 				'depmod.js',
 				'node_modules/redirect-exports'
 			].sort());
 			data.should.have.property('js');
-		}).finally(() => {
+		} finally {
 			delete process.env.BROWSERSLIST;
-		});
+		}
 	});
 
-	it('should support legacy-resolved jquery-like with ignored file', function () {
+	it('should support legacy-resolved jquery-like with ignored file', async function () {
 		process.env.BROWSERSLIST = "last 1 chrome version";
-		return bundledom('test/fixtures/legacy2.html', {
-			root: "test/fixtures",
-			modulesPrefix: '/',
-			modulesRoot: "test",
-			exclude: [],
-			concatenate: true
-		}).then(function (data) {
+		try {
+			const data = await bundledom('test/fixtures/legacy2.html', {
+				root: "test/fixtures",
+				modulesPrefix: '/',
+				modulesRoot: "test",
+				exclude: [],
+				concatenate: true
+			});
 			data.should.have.property('js');
 			data.scripts.should.eql([
 				'fakejquery.js', 'usejquery.js', 'mod.js', 'depmod.js'
 			]);
-
-		}).finally(() => {
+		} finally {
 			delete process.env.BROWSERSLIST;
-		});
+		}
 	});
 
-	it('should order scripts w.r.t. defer, module, or nothing', function () {
+	it('should order scripts w.r.t. defer, module, or nothing', async function () {
 		process.env.BROWSERSLIST = "last 1 chrome version";
-		return bundledom('test/fixtures/legacy3.html', {
-			root: "test/fixtures",
-			modulesPrefix: '/',
-			modulesRoot: "test",
-			exclude: [],
-			concatenate: true,
-			js: '../bundles/legacy3.js'
-		}).then(function (data) {
+		try {
+			const data = await bundledom('test/fixtures/legacy3.html', {
+				root: "test/fixtures",
+				modulesPrefix: '/',
+				modulesRoot: "test",
+				exclude: [],
+				concatenate: true,
+				js: '../bundles/legacy3.js'
+			});
 			data.should.have.property('js');
 			data.scripts.should.eql([
 				'usejquery.js', 'fakejquery.js', 'mod.js', 'depmod.js'
 			]);
 			data.html.should.containEql('../bundles/legacy3.js');
 			data.html.should.containEql('defer=""');
-
-		}).finally(() => {
+		} finally {
 			delete process.env.BROWSERSLIST;
-		});
+		}
 	});
 
-	it('should ignore a script', function () {
-		return bundledom('test/fixtures/exclude.html', {
+	it('should ignore a script', async function () {
+		const data = await bundledom('test/fixtures/exclude.html', {
 			ignore: ['b.js']
-		}).then(function (data) {
-			data.should.have.property('js');
-			data.should.have.property('css');
-			data.should.have.property('html');
-			data.html.indexOf('<script src="b.js"></script>').should.be.greaterThan(0);
 		});
+		data.should.have.property('js');
+		data.should.have.property('css');
+		data.should.have.property('html');
+		data.html.indexOf('<script src="b.js"></script>').should.be.greaterThan(0);
 	});
 
-	it('should ignore a script using a wildcard', function () {
-		return bundledom('test/fixtures/exclude.html', {
+	it('should ignore a script using a wildcard', async function () {
+		const data = await bundledom('test/fixtures/exclude.html', {
 			ignore: ['*.js']
-		}).then(function (data) {
-			data.should.have.property('js');
-			data.should.have.property('css');
-			data.should.have.property('html');
-			data.html.indexOf('<script src="b.js"></script>').should.be.greaterThan(0);
 		});
+		data.should.have.property('js');
+		data.should.have.property('css');
+		data.should.have.property('html');
+		data.html.indexOf('<script src="b.js"></script>').should.be.greaterThan(0);
 	});
 
-	it('should bundle html import and run it', function () {
+	it('should bundle html import and run it', async function () {
 		const filepath = 'test/fixtures/import.html';
-		return bundledom(filepath).then(function (data) {
-			data.should.have.property('js');
-			data.should.have.property('css');
-			data.should.have.property('html');
-			return runDom(filepath, data).then(function (doc) {
-				should.exist(doc.querySelector('head > style'));
-				should.exist(doc.querySelector('body > .element'));
-			});
-		});
+		const data = await bundledom(filepath);
+		data.should.have.property('js');
+		data.should.have.property('css');
+		data.should.have.property('html');
+		const doc = await runDom(filepath, data);
+		should.exist(doc.querySelector('head > style'));
+		should.exist(doc.querySelector('body > .element'));
 	});
 
-	it('should bundle html import in html import and run it', function () {
+	it('should bundle html import in html import and run it', async function () {
 		const filepath = 'test/fixtures/import-in-import.html';
-		return bundledom(filepath).then(function (data) {
-			data.should.have.property('js');
-			data.should.have.property('css');
-			data.should.have.property('html');
-			return runDom(filepath, data).then(function (doc) {
-				should.exist(doc.querySelector('head > style'));
-				should.exist(doc.querySelector('body > .element'));
-			});
-		});
+		const data = await bundledom(filepath);
+		data.should.have.property('js');
+		data.should.have.property('css');
+		data.should.have.property('html');
+		const doc = await runDom(filepath, data);
+		should.exist(doc.querySelector('head > style'));
+		should.exist(doc.querySelector('body > .element'));
 	});
 
-	it('should bundle imported element with inner imported element and run it', function () {
+	it('should bundle imported element with inner imported element and run it', async function () {
 		const filepath = 'test/fixtures/element-in-element.html';
-		return bundledom(filepath).then(function (data) {
-			data.should.have.property('js');
-			data.should.have.property('css');
-			data.should.have.property('html');
-			return runDom(filepath, data).then(function (doc) {
-				should.exist(doc.querySelector('head > style'));
-				should.exist(doc.querySelector('body > .superelement'));
-				should.exist(doc.querySelector('body > .element'));
-				doc.querySelector('body > .element').innerHTML.should.match(/test1\n\s+test2/);
-			});
-		});
+		const data = await bundledom(filepath);
+		data.should.have.property('js');
+		data.should.have.property('css');
+		data.should.have.property('html');
+		const doc = await runDom(filepath, data);
+		should.exist(doc.querySelector('head > style'));
+		should.exist(doc.querySelector('body > .superelement'));
+		should.exist(doc.querySelector('body > .element'));
+		doc.querySelector('body > .element').innerHTML.should.match(/test1\n\s+test2/);
 	});
 
-	it('should bundle html import with sub import from another dir', function () {
-		return Promise.all([
+	it('should bundle html import with sub import from another dir', async function () {
+		await Promise.all([
 			copyOver('test/fixtures/sub/sub.html', 'test/bundles/sub/sub.html'),
 			copyOver('test/fixtures/sub/sub.js', 'test/bundles/sub/sub.js'),
 			copyOver('test/fixtures/sub/sub.css', 'test/bundles/sub.css')
-		]).then(function () {
-			return bundledom('test/fixtures/import-sub.html', {
-				root: 'test/bundles',
-				html: 'import-sub.html',
-				js: 'import-sub.js'
-			}).then(function (data) {
-				data.should.have.property('js');
-				return new Promise(function (resolve, reject) {
-					fs.readFile('test/bundles/import-sub.js', function (err, data) {
-						if (err) return reject(err);
-						data.toString().should.match(/.*window\.test=23.*/);
-						data.toString().should.match(/.*mysubselector.*/);
-						return resolve();
-					});
-				});
-			});
+		]);
+
+		const data = await bundledom('test/fixtures/import-sub.html', {
+			root: 'test/bundles',
+			html: 'import-sub.html',
+			js: 'import-sub.js'
 		});
+		data.should.have.property('js');
+		const str = (await fs.readFile('test/bundles/import-sub.js')).toString();
+		str.should.match(/.*window\.test=23.*/);
+		str.should.match(/.*mysubselector.*/);
 	});
 
-	it('should not bundle remotes', function () {
-		return bundledom('test/fixtures/remote.html', {
+	it('should not bundle remotes', async function () {
+		const data = await bundledom('test/fixtures/remote.html', {
 			root: 'test/bundles',
 			html: 'remote.html',
 			css: 'remote.css'
-		}).then(function (data) {
-			data.should.have.property('css');
-			return new Promise(function (resolve, reject) {
-				fs.readFile('test/bundles/remote.css', function (err, data) {
-					if (err) return reject(err);
-					data.toString().should.not.containEql("font-family");
-					return resolve();
-				});
-			});
 		});
+		data.should.have.property('css');
+		const str = (await fs.readFile('test/bundles/remote.css')).toString();
+		str.should.not.containEql("font-family");
 	});
 
-	it('should bundle remote stylesheet', function () {
+	it('should bundle remote stylesheet', async function () {
 		this.timeout(10000);
-		return bundledom('test/fixtures/remote.html', {
+		const data = await bundledom('test/fixtures/remote.html', {
 			root: 'test/bundles',
 			html: 'remote.html',
 			css: 'remote.css',
 			remotes: ['fonts.googleapis.com'],
 			concatenate: true
-		}).then(function (data) {
-			data.should.have.property('css');
-			return new Promise(function (resolve, reject) {
-				fs.readFile('test/bundles/remote.css', function (err, data) {
-					if (err) return reject(err);
-					data.toString().should.containEql("font-family");
-					return resolve();
-				});
-			});
 		});
+		data.should.have.property('css');
+		const str = (await fs.readFile('test/bundles/remote.css')).toString();
+		str.should.containEql("font-family");
 	});
 
-	it('should bundle stylesheet from a module and url to non css files alone', function () {
+	it('should bundle stylesheet from a module and url to non css files alone', async function () {
 		this.timeout(10000);
-		return bundledom('test/fixtures/style.html', {
+		const data = await bundledom('test/fixtures/style.html', {
 			concatenate: true,
 			modulesPrefix: "/",
 			modulesRoot: "test"
-		}).then(function (data) {
-			data.assets.should.eql(['../node_modules/style/fonts/test.ttf']);
-			data.stylesheets.should.eql(['node_modules/style']);
-			data.css.should.containEql("url('../node_modules/style/fonts/test.ttf')");
-			data.css.should.containEql("-webkit-animation-duration: 12ms");
 		});
+		data.assets.should.eql(['../node_modules/style/fonts/test.ttf']);
+		data.stylesheets.should.eql(['node_modules/style']);
+		data.css.should.containEql("url('../node_modules/style/fonts/test.ttf')");
+		data.css.should.containEql("-webkit-animation-duration: 12ms");
 	});
 
-	it('should bundle stylesheet from a module and copy assets to dir', function () {
+	it('should bundle stylesheet from a module and copy assets to dir', async function () {
 		this.timeout(10000);
-		return bundledom('test/fixtures/style.html', {
+		const data = await bundledom('test/fixtures/style.html', {
 			modulesPrefix: "/",
 			modulesRoot: "test",
 			assets: "assets",
 			css: "css/subcss/style.css",
 			root: "test/bundles"
-		}).then(function (data) {
-			data.assets.should.eql(["../node_modules/style/fonts/test.ttf"]);
-			data.stylesheets.should.eql(['node_modules/style']);
-			data.css.should.containEql("src:url(../../assets/68a581f6.ttf)");
-			data.css.should.containEql("-webkit-animation-duration:12ms");
-			fs.existsSync("test/bundles/assets/68a581f6.ttf").should.be.ok();
 		});
+		data.assets.should.eql(["../node_modules/style/fonts/test.ttf"]);
+		data.stylesheets.should.eql(['node_modules/style']);
+		data.css.should.containEql("src:url(../../assets/68a581f6.ttf)");
+		data.css.should.containEql("-webkit-animation-duration:12ms");
+		await fs.stat("test/bundles/assets/68a581f6.ttf");
 	});
 
-	it('should import jquery-like bundle with side effects', function () {
-		return bundledom('test/fixtures/fakejquery.html', {
+	it('should import jquery-like bundle with side effects', async function () {
+		const data = await bundledom('test/fixtures/fakejquery.html', {
 			concatenate: true
-		}).then(function (data) {
-			data.js.should.containEql("window.$ = jQuery");
-			data.js.should.containEql("window.$()");
 		});
+		data.js.should.containEql("window.$ = jQuery");
+		data.js.should.containEql("window.$()");
 	});
 
-	it('should resolve org modules', function () {
-		return bundledom('test/fixtures/orgmodule.html', {
+	it('should resolve org modules', async function () {
+		const data = await bundledom('test/fixtures/orgmodule.html', {
 			concatenate: true,
 			modulesRoot: "test"
-		}).then(function (data) {
-			data.js.should.containEql("window.test = 1");
 		});
+		data.js.should.containEql("window.test = 1");
 	});
 
-	it('should bundle remote script', function () {
+	it('should bundle remote script', async function () {
 		this.timeout(10000);
-		return Promise.all([
-			copyOver('test/fixtures/usejquery.js', 'test/bundles/usejquery.js')
-		]).then(function () {
-			return bundledom('test/fixtures/remote.html', {
-				root: 'test/bundles',
-				html: 'remote.html',
-				js: 'remote.js',
-				remotes: ['ajax.googleapis.com']
-			}).then(function (data) {
-				data.should.have.property('js');
-				return new Promise(function (resolve, reject) {
-					fs.readFile('test/bundles/remote.js', function (err, data) {
-						if (err) return reject(err);
-						data.toString().should.containEql("jQuery");
-						return resolve();
-					});
-				});
-			});
+		await copyOver('test/fixtures/usejquery.js', 'test/bundles/usejquery.js');
+
+		const data = await bundledom('test/fixtures/remote.html', {
+			root: 'test/bundles',
+			html: 'remote.html',
+			js: 'remote.js',
+			remotes: ['ajax.googleapis.com']
 		});
+		data.should.have.property('js');
+
+		const str = (await fs.readFile('test/bundles/remote.js')).toString();
+		str.should.containEql("jQuery");
 	});
 
 });
 
-function copyOver(from, to) {
-	return new Promise(function (resolve, reject) {
-		fs.unlink(to, function (err) {
-			fs.readFile(from, function (err, data) {
-				if (err) return reject(err);
-				mkdirp(Path.dirname(to)).then(function () {
-					fs.writeFile(to, data, function (err) {
-						if (err) return reject(err);
-						resolve();
-					});
-				}).catch(reject);
-			});
-		});
+async function copyOver(from, to) {
+	await fs.unlink(to);
+	const data = await fs.readFile(from);
+	await mkdirp(Path.dirname(to));
+	await fs.writeFile(to, data);
+}
+
+async function runDom(htmlPath, data) {
+	const virtualConsole = new jsdom.VirtualConsole();
+	virtualConsole.on('jsdomError', function (err) {
+		throw err;
 	});
+	const dom = new jsdom.JSDOM(data.html, {
+		virtualConsole: virtualConsole,
+		url: 'file://' + Path.resolve(htmlPath),
+		runScripts: "dangerously",
+		resources: "usable"
+	});
+	dom.window.eval(data.js);
+	return dom.window.document;
 }
